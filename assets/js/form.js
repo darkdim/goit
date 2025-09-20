d031a50adaf39049d1e9be798d3b99fb7b521595
Dmytro Porokhnya <dmporokhnya@gmail.com>
2025-09-20 18:12:24 +0300

    fixed anim

--- assets/js/form.js
+++ assets/js/form.js
@@ -30,7 +30,7 @@ function addGooseElement() {
 function showGooseAnim() {
     const gooseEl = document.querySelector('.gus-anim');
 
-    gooseEl.setAttribute('src', './img/gus-anim.gif');
+    gooseEl.setAttribute('src', 'assets/img/gus-anim.gif');
     
 
     setTimeout(() => {
